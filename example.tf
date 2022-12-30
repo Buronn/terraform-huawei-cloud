dada5ee5de6a2b38172b24e30f910dbbeb0282bf
Fernando Buron <fernando.buron@mail.udp.cl>
2022-12-31 02:14:32 +0800

    dns record

--- example.tf
+++ example.tf
@@ -10,8 +10,21 @@ terraform {
 # Configure the HuaweiCloud Provider
 provider "huaweicloud" {}
 
-# Create a VPC
-resource "huaweicloud_vpc" "example" {
-  name = "my_vpc"
-  cidr = "192.168.0.0/16"
+
+# ----- Create DNS and Record Set -----
+resource "huaweicloud_dns_zone" "example_zone" {
+  name        = "tf.buron.social"
+  email       = "fernando.buron@mail.udp.cl"
+  description = "buenastardes"
+  ttl         = 6000
+  zone_type   = "public"
+}
+
+resource "huaweicloud_dns_recordset" "dns_recordset" {
+  zone_id     = huaweicloud_dns_zone.example_zone.id
+  name        = "tf.buron.social."
+  description = "DNS Record from Terraform"
+  ttl         = 3000
+  type        = "A"
+  records     = ["159.138.115.199"]
 }
