16a05607ffbd73fc651474aaf2ab912c48141a94
Fernando Buron <fernando.buron@mail.udp.cl>
2022-12-31 00:43:43 +0800

    vpc test

--- example.tf
+++ example.tf
@@ -1,1 +1,17 @@
+terraform {
+  required_providers {
+    huaweicloud = {
+      source  = "huaweicloud/huaweicloud"
+      version = "~> 1.26.0"
+    }
+  }
+}
+
+# Configure the HuaweiCloud Provider
 provider "huaweicloud" {}
+
+# Create a VPC
+resource "huaweicloud_vpc" "example" {
+  name = "my_vpc"
+  cidr = "192.168.0.0/16"
+}
