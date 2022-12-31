669b21f665abb090fa2f9b721ea66f898e2d3d4d
Fernando Buron <fernando.buron@mail.udp.cl>
2022-12-31 08:04:41 +0800

    separated targets

---
+++ WAF/example.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    huaweicloud = {
+      source  = "huaweicloud/huaweicloud"
+      version = ">= 1.26.0"
+    }
+  }
+}
+
+# Configure the HuaweiCloud Provider
+provider "huaweicloud" {}
+
+variable "domain_name" {
+  default = "tf.buron.social"
+}
+variable "origin_server" {
+  default = "159.138.115.199"
+}
+
+# ----- Create DNS and Record Set ----- https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs/resources/dns_ptrrecord
+resource "huaweicloud_dns_zone" "example_zone" {
+  name        = var.domain_name
+  email       = "fernando.buron@mail.udp.cl"
+  description = "DNS Public Zone created from Terraform"
+  ttl         = 6000
+  zone_type   = "public"
+}
+
+resource "huaweicloud_dns_recordset" "dns_recordset" {
+  zone_id     = huaweicloud_dns_zone.example_zone.id
+  name        = "tf.buron.social."
+  description = "DNS Record from Terraform"
+  ttl         = 3000
+  type        = "A"
+  records     = [var.origin_server]
+}
+
+
+# ----- Create CDN domain ----- https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs/resources/cdn_domain
+
+
+# ----- Configure CDN domain ----- 
+resource "huaweicloud_cdn_domain" "domain_1" {
+  name = var.domain_name
+  type = "web"
+  service_area = "outside_mainland_china"
+
+  sources {
+    origin      = var.origin_server
+    origin_type = "ipaddr"
+    active      = 1
+  }
+
+  cache_settings {
+    rules {
+      rule_type = 0
+      ttl       = 180
+      ttl_type  = 4
+      priority  = 2
+    }
+  }
+}
+
+# ----- Create WAF domain ----- https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs/resources/waf_dedicated_instance
+
